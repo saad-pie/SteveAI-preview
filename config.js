612dc90e92f57350f2744ca3ffd9c0dcc4561db0
saad-pie <saadabdulrehman7474@gmail.com>
2025-11-22 23:17:46 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ const proxiedURL = (base) => PROXY + encodeURIComponent(base);
 
 // Two API keys as fallback
 const API_KEYS = [
-  "ddc-a4f-d61cbe09b0f945ea93403a420dba8155",
-  "ddc-a4f-93af1cce14774a6f831d244f4df3eb9e"
+  "ddc-a4f-93af1cce14774a6f831d244f4df3eb9e",
+  "ddc-a4f-d61cbe09b0f945ea93403a420dba8155"
 ];
 
 // Combine all values into a single object
